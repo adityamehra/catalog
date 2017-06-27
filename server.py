d1addee3bf9dba9aab76934327464020b91b8d5f
Aditya Mehra <mehraaditya71@gmail.com>
2017-06-26 19:32:24 -0700

    modified server.py

--- server.py
+++ server.py
@@ -4,7 +4,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
 
+#import for create login request on client side
+from flask import session as login_session
+import random
+import string
+
+# import to handle login request on server side
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response, flash
+import requests
+
+
 app = Flask('__name__')
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Catalog"
 
 engine = create_engine('sqlite:///catalog.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -18,12 +36,143 @@ session = DBSession()
 def getCategories():
     return session.query(Category).all()
 
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # print credentials.to_json()
+    # print credentials
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    # print access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # print result
+    # print result.get('error')
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        # print "if result.get('error') is not None:"
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    # print gplus_id
+    if result['user_id'] != gplus_id:
+        # print "if result['user_id'] != gplus_id:"
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        # print "if result['issued_to'] != CLIENT_ID:"
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        # print "if stored_credentials is not None and gplus_id == stored_gplus_id:"
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    return output
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session['access_token']
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    if access_token is None:
+ 	print 'Access Token is None'
+    	response = make_response(json.dumps('Current user not connected.'), 401)
+    	response.headers['Content-Type'] = 'application/json'
+    	return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        name = login_session['username']
+    	del login_session['username']
+    	del login_session['email']
+    	del login_session['picture']
+        # Re-direct to /catalog when disconnect
+        return redirect('/catalog')
+    else:
+    	response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+    	response.headers['Content-Type'] = 'application/json'
+    	return response
 
 @app.route('/')
 @app.route('/catalog')
 @app.route('/catalog/')
 def showCatalog():
-    return render_template('index.html', categories=getCategories())
+    if 'username' not in login_session:
+        return render_template('index_signin.html', categories=getCategories())
+    else:
+        return render_template('index_signout.html', categories=getCategories(), login_session=login_session)
 
 @app.route('/catalog/<int:category_id>/')
 @app.route('/catalog/<int:category_id>/items/')
@@ -31,7 +180,11 @@ def showItems(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(
         category_id=category_id).all()
-    return render_template('items.html', items=items, category=category)
+    if 'username' not in login_session:
+        return render_template('items_signin.html', items=items, category=category)
+    else:
+        return render_template('items_signout.html', items=items, category=category, login_session=login_session)
+
 
 @app.route('/catalog/<int:category_id>/JSON')
 @app.route('/catalog/<int:category_id>/items/JSON')
@@ -40,20 +193,18 @@ def categoryMenuJSON(category_id):
     items = session.query(Item).filter_by(category_id=category_id).all()
     return jsonify(Items=[i.serialize for i in items])
 
-
-@app.route('/category/<int:category_id>/menu/<int:menu_id>/JSON')
-def menuItemJSON(category_id, menu_id):
-    Item = session.query(Item).filter_by(id=menu_id).one()
-    return jsonify(Menu_Item=Menu_Item.serialize)
-
+@app.route('/catalog/<int:category_id>/item/<int:item_id>/JSON')
+def menuItemJSON(category_id, item_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    item = session.query(category).filter_by(id=item_id).one()
+    return jsonify(Item=Item.serialize)
 
 @app.route('/catalog/JSON')
 def categoriesJSON():
     categories = session.query(Category).all()
     return jsonify(Catalog=[r.serialize for r in categories])
 
-
-
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
